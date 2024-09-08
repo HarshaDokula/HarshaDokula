2eb6d68c52f891a717a8ffdb79c4f42dcfb56dca
HarshaDokula <harshadokula@gmail.com>
2024-09-08 19:26:35 -0400

    converted analytics to js

---
+++ gtag.js
@@ -0,0 +1,15 @@
+// Google tag (gtag.js)
+(function() {
+    var script = document.createElement('script');
+    script.async = true;
+    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-JGZL35DMP0';
+    document.head.appendChild(script);
+})();
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){
+    dataLayer.push(arguments);
+}
+
+gtag('js', new Date());
+gtag('config', 'G-JGZL35DMP0');
