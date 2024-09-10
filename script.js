4c9415c6cddb9aab7d460fac6ede36f38b9b64db
HarshaDokula <harshadokula@gmail.com>
2024-09-10 01:59:20 -0400

    fixed images

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ window.addEventListener("load", function() {
     const images = document.querySelectorAll(".cert-image");
     images.forEach(function(img) {
         img.onerror = function() {
-            img.style.display = "none";  // Hide the image if it doesn't load
+            // Hide the entire parent container of the image if it fails to load
+            img.parentElement.style.display = "none";
         };
     });
 });
+
